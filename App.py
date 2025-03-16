23d609a06b78405f2eb409997866db9c88b423c1
jaxchoi <jaxcho1118@gmail.com>
2025-03-16 12:48:21 -0700

    Added supabase

--- App.py
+++ App.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+from supabase import create_client
 
+load_dotenv()
+
+url = os.environ.get('SUPABASE_URL')
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
 
 app=Flask(__name__)
 
